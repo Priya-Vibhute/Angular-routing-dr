74fb1b3f4dac890a3577da13edb1bf782087a921
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-06-01 12:45:04 +0530

    1 may 2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'products',component:ProductComponent},
+    {path:'profile',component:ProfileComponent},
+    {path:'dashboard',component:DashboardComponent}
+];
